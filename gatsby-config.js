bde1ee868b7822cb09095cae6f7570080d86a167
fuerte-nerd <fuertenerd@gmail.com>
2020-02-12 09:39:08 +0000

    added netlifycms dependencies to plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,8 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -26,7 +28,7 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
